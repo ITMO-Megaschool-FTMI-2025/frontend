d20f8a8e74d0d40722ab9b534a723d4d688cb72a
Mikhail The Greatest I <zdarovayrodi@icloud.com>
2025-01-31 13:32:58 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,15 +25,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-auto`}
       >
-        {/* Blurred spheres */}
-        <div className="absolute inset-0 overflow-hidden pointer-events-none">
-          <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] rounded-full bg-green-500 opacity-30 blur-[100px]" />
-          <div className="absolute bottom-[-10%] right-[-10%] w-[60%] h-[60%] rounded-full bg-purple-500 opacity-30 blur-[100px]" />
-          <div className="absolute top-[30%] right-[-5%] w-[40%] h-[40%] rounded-full bg-orange-300 opacity-30 blur-[100px]" />
+        <div className="relative min-h-screen overflow-hidden">
+          {/* Blurred spheres */}
+          <div className="absolute inset-0 overflow-x-hidden pointer-events-none">
+            <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] rounded-full bg-green-500 opacity-30 blur-[100px]" />
+            <div className="absolute top-[-30%] right-[-10%] w-[60%] h-[60%] rounded-full bg-purple-500 opacity-30 blur-[100px]" />
+            {/* <div className="absolute top-[30%] right-[-5%] w-[40%] h-[40%] rounded-full bg-orange-300 opacity-30 blur-[100px]" /> */}
+          </div>
+          {children}
         </div>
-        {children}
       </body>
     </html>
   );
