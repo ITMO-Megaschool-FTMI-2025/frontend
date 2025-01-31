95ae41898e74b952fa326c21b8f69273b87d2fa6
Mikhail The Greatest I <zdarovayrodi@icloud.com>
2025-01-31 12:38:24 +0300

    feat(blobs): add blurred blobs to bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Blurred spheres */}
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <div className="absolute top-[-10%] left-[-10%] w-[50%] h-[50%] rounded-full bg-green-500 opacity-30 blur-[100px]" />
+          <div className="absolute bottom-[-10%] right-[-10%] w-[60%] h-[60%] rounded-full bg-purple-500 opacity-30 blur-[100px]" />
+          <div className="absolute top-[30%] right-[-5%] w-[40%] h-[40%] rounded-full bg-orange-300 opacity-30 blur-[100px]" />
+        </div>
         {children}
       </body>
     </html>
